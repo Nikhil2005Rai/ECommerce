af5cab0d82cd5164556e6601dda73401c6c0c6ed
Nikhil2005Rai <n2005rai@gmail.com>
2024-07-19 10:03:30 +0530

    Romoved tailwind

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,65 @@
 import React from "react";
 import Navbar from "./components/Navbar/Navbar";
+import Shop from "./pages/Shop";
+import ShopCategory from "./pages/ShopCategory";
+import Product from "./pages/Product";
+import Cart from "./pages/Cart";
+import LoginSignup from "./pages/LoginSignup";
+import { RouterProvider, createBrowserRouter, Outlet } from "react-router-dom";
+
+const Layout = () => {
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+    </>
+  );
+};
 
 const App = () => {
-  return <Navbar />;
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <Shop />,
+        },
+        {
+          path: "/mens",
+          element: <ShopCategory category="men" />,
+        },
+        {
+          path: "/womens",
+          element: <ShopCategory category="women" />,
+        },
+        {
+          path: "/kids",
+          element: <ShopCategory category="kid" />,
+        },
+        {
+          path: "/product",
+          element: <Product />,
+          children: [
+            {
+              path: ":productId",
+              element: <Product />,
+            },
+          ],
+        },
+        {
+          path: "/cart",
+          element: <Cart />,
+        },
+        {
+          path: "/login",
+          element: <LoginSignup />,
+        },
+      ],
+    },
+  ]);
+  return <RouterProvider router={router} />;
 };
 
 export default App;
--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,70 +1,75 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import logo from "../../assets/logo.png";
 import cart_icon from "../../assets/cart_icon.png";
+import { Link } from "react-router-dom";
+import './Navbar.css'
 
 const Navbar = () => {
   const [menu, setMenu] = useState("shop");
 
   return (
-    <div className="flex justify-around p-4 shadow-md">
-      <div className="flex items-center gap-2.5">
-        <img src={logo} alt="" className="w-12" />
-        <p className="text-gray-800 text-2xl font-medium">SHOPPER</p>
+    <div className="navbar">
+      <div className="nav-logo">
+        <img src={logo} alt="" className="" />
+        <p className="">SHOPPER</p>
       </div>
-      <ul className="flex items-center gap-12 text-gray-600 text-lg font-medium ">
+      <ul className="nav-menu">
         <li
-          className="flex-col justify-center items-center  cursor-pointer"
+          className=""
           onClick={() => {
             setMenu("shop");
-            console.log(menu);
           }}
         >
-          Shop
-          {menu === "shop" && (
-            <hr className="bg-red-500 h-1  rounded-xl mt-2" />
-          )}
+          <Link to="/" style={{ all: "unset" }}>
+            Shop
+          </Link>
+
+          {menu === "shop" && <hr />}
         </li>
         <li
           className="flex-col justify-center items-center  cursor-pointer"
           onClick={() => {
             setMenu("men");
-            console.log(menu);
           }}
         >
-          Men
+          <Link to="/mens" style={{ all: "unset" }}>
+            Men
+          </Link>
           {menu === "men" && <hr className="bg-red-500 h-1  rounded-xl mt-2" />}
         </li>
         <li
-          className="flex-col justify-center items-center cursor-pointer"
+          className=""
           onClick={() => {
             setMenu("women");
           }}
         >
-          Women
-          {menu === "women" && (
-            <hr className="bg-red-500 h-1  rounded-xl mt-2" />
-          )}
+          <Link to="/womens" style={{ all: "unset" }}>
+            Women
+          </Link>
+
+          {menu === "women" && <hr />}
         </li>
         <li
-          className="flex-col justify-center items-center cursor-pointer"
+          className=""
           onClick={() => {
             setMenu("kids");
           }}
         >
-          Kids
-          {menu === "kids" && (
-            <hr className="bg-red-500 h-1  rounded-xl mt-2" />
-          )}
+          <Link to="/kids" style={{ all: "unset" }}>
+            Kids
+          </Link>
+          {menu === "kids" && <hr />}
         </li>
       </ul>
-      <div className="flex items-center gap-12">
-        <button className="w-32 h-12 border border-gray-600 rounded-full text-gray-600 text-lg font-medium bg-white cursor-pointer hover:bg-gray-100 hover:text-gray-700 focus:outline-none transition duration-300 ease-in-out">
-          Login
-        </button>
-        <img src={cart_icon} alt="" className="w-8" />
-        <div className="w-5 h-5 flex items-center justify-center -mt-8 -ml-14 rounded-xl size-4 bg-red-600 text-white">
-          0
-        </div>
+      <div className="nav-login-cart">
+        <Link to="/login">
+          <button className="">Login</button>
+        </Link>
+
+        <Link to="/cart">
+          <img src={cart_icon} alt="" className="w-8" />
+        </Link>
+        <div className="nav-cart-count">0</div>
       </div>
     </div>
   );
