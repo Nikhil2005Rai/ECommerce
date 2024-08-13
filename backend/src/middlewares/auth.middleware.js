e0c6b5c92471ade22d0acaa7cde5e73e5bbabebc
Nikhil2005Rai <n2005rai@gmail.com>
2024-08-13 11:17:45 +0530

    test

--- backend/src/middlewares/auth.middleware.js
+++ backend/src/middlewares/auth.middleware.js
@@ -6,9 +6,10 @@ import { User } from "../models/user.model.js";
 export const verifyJWT = asyncHandler(async (req, _, next) => {
   try {
     const token =
-      req.cookies?.accessToken ||
-      req.header("Authorization")?.replace("Bearer ", "");
-
+    req.cookies?.accessToken ||
+    req.header("Authorization")?.replace("Bearer ", "");
+    
+    console.log(token)
     if (!token) {
       throw new ApiError(401, "Unauthorized request");
     }
